318713e7b5cb83781606e51cb168e7292343b92f
Adam Alton <adamalton@gmail.com>
2016-03-28 01:05:57 +0100

    Remove `expiry` field, as it's fairly pointless.

--- letsencryptae/models.py
+++ letsencryptae/models.py
@@ -1,21 +1,10 @@
-# STANDARD LIB
-from datetime import timedelta
-
 # THIRD PARTY
 from djangae.fields import CharField
 from django.db import models
-from django.utils import timezone
 
-# LETS ENCRYPT APP ENGINE
-from letsencryptae.constants import DEFAULT_CERT_VALIDITY_DAYS
-
-
-def probable_expiry():
-    return timezone.now() + timedelta(days=DEFAULT_CERT_VALIDITY_DAYS)
 
 class Secret(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    expiry = models.DateTimeField(default=probable_expiry)
     url_slug = CharField(primary_key=True)
     secret = CharField()
 
