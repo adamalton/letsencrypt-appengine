59b45ab2a63bf628aa027e3094befb1878a43c7b
Adam Alton <adamalton@gmail.com>
2016-03-28 01:08:30 +0100

    Fixing regex in URL pattern.

--- letsencryptae/urls.py
+++ letsencryptae/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     'letsencryptae.views',
-    url(r'^\.well-known/acme-challenge/(?P<url_slug>[a-zA-z0-9_-]+$', 'secret', name='secret'),
+    url(r'^\.well-known/acme-challenge/(?P<url_slug>[a-zA-z0-9_-]+)$', 'secret', name='secret'),
 )
