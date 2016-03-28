1233a221bf124491966f5e9aa8bbc492e3896cc9
Adam Alton <adamalton@gmail.com>
2016-03-28 01:03:02 +0100

    Display created dates of Secrets in Django admin list view.

--- letsencryptae/admin.py
+++ letsencryptae/admin.py
@@ -4,4 +4,9 @@ from django.contrib import admin
 # LETS ENCRYPT APP ENGINE
 from letsencryptae.models import Secret
 
-admin.site.register(Secret)
+
+class SecretAdmin(admin.ModelAdmin):
+    list_display = ('url_slug', 'created')
+
+
+admin.site.register(Secret, SecretAdmin)
